a6c7b6d61fb3257a4ddcabd1627213ed0606bc72
KwameNtaadu007 <kwamealabama@gmail.com>
2023-02-17 22:55:27 -0800

    Betty corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,24 +4,24 @@
 
 /**
  * main - prints all possible different combinations of two digits
- * 
+ *
  * Return: Always 0 (Success)
  */
- 
+
 int main(void)
 {
-int x,y;
-    
-	for(x = '0'; x < '9'; x++)
+int x, y;
+
+	for (x = '0'; x < '9'; x++)
 	{
-		for(y = x +1; y <= '9'; y++)
+		for (y = x + 1; y <= '9'; y++)
 		{
-			if(y != x)
+			if (y != x)
 			{
 				putchar(x);
 				putchar(y);
 
-				if(x == '8' && y == '9')
+				if (x == '8' && y == '9')
 					continue;
 				putchar(',');
 				putchar(' ');
