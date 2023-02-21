ae77c92afd1838da680e701963bf3641153a3fb6
KwameNtaadu007 <kwamealabama@gmail.com>
2023-02-21 07:30:16 -0800

    _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * prints _putchar, followed by a new line.
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char strng[] = "_putchar";
+
+	printf("%s\n",strng);
+
+	return (0);
+}
