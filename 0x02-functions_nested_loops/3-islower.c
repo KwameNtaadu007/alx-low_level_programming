a17316e389861db3c076dc2a3993c46321c042ad
KwameNtaadu007 <kwamealabama@gmail.com>
2023-02-21 15:18:01 -0800

    _islower description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Checks for lowercase character
+ * _islower: Checks for lowercase character
  * @c: The character to be checked
  * Return : 1 for lowercase or 0 for any other thing
  */
