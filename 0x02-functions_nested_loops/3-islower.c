3e1d2a70561ac21a5f32dc1353ffff01b6c4652b
KwameNtaadu007 <kwamealabama@gmail.com>
2023-02-21 15:13:02 -0800

    betty space check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - Checks for lowercase character 
- * @c: The character to be checked 
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked
  * Return : 1 for lowercase or 0 for any other thing
  */
 
@@ -10,7 +10,7 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-	return(1);
+	return (1);
 	}
-	return(0);
+	return (0);
 }
