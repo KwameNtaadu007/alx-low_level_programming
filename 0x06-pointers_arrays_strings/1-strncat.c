090501bb8797125adf370c7c49824f09e5e3adda
KwameNtaadu007 <kwamealabama@gmail.com>
2023-03-02 10:52:46 -0800

    String concatenation correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,16 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
-
 	while (dest[i] != '\0')
 		i++;
 
 	while (j < n)
 	{
-		dest[i] = src[j];
-		i++;
+		dest[i + j] = src[j];
 		j++;
 	}
 
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 
 }
