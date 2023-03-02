64f9e282b1802b73dd68fc5a878156bc74d62d27
KwameNtaadu007 <kwamealabama@gmail.com>
2023-03-02 02:37:37 -0800

    0-strcat correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	char *temp = dest;
 
-	for (i = 0; dest[i]; i++)
+	while (*dest)
+		dest++;
+	while ((*dest++ = *src++))
 		;
-	for (j = 0; src[j]; j++)
-		dest[i + j] = src[j];
-	dest[i + j] = '\0';
-	return (dest);
+		return (temp);
 }
 
