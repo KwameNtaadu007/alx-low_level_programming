46bec9d14fa9f32031cdb6902b2f32730e930028
KwameNtaadu007 <kwamealabama@gmail.com>
2023-03-02 21:41:38 -0800

    ROT

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ * Return: pointer to encoded string
+ */
+char *rot13(char *s)
+{
+	int i = 0, j = 0;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i])
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	i++;
+	}
+
+	return (s);
+}
