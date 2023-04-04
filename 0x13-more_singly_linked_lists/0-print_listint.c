dfe12aff07e0da1fb301672753a16f898b1ff8aa
KwameNtaadu007 <kwamealabama@gmail.com>
2023-04-04 04:05:46 -0700

    print the number of nodes of a singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+
+/**
+ * print_listint - function to print singly linked list
+ * @count: integer to hold number of nodes
+ * @h: points to the head of the first node
+ *
+ * Return: counted number of nodes
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		printf("Empty linked list");
+	}
+
+	while (h != NULL)
+	{
+		printf("Element [%d] - %d\n", count, h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
