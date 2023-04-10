3a0db70cafd0468d9fe4b385f0a2a783606e9c5e
KwameNtaadu007 <kwamealabama@gmail.com>
2023-04-10 14:21:45 -0700

    correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,11 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-    {
-        return -1; /*index out of bounds*/
-    }
-    *n |= (1UL << index);
-    return (1);
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1); /*index out of bounds*/
+	}
+	*n |= (1UL << index);
+	return (1);
 }
-
