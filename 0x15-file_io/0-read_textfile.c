c83907888c54e264e6b7879ba72da3adf92c17da
KwameNtaadu007 <kwamealabama@gmail.com>
2023-04-10 05:36:16 -0700

    Read a text file and print it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: text file to be read
+ * @letters: numbers count of the text in the file
+ *
+ * Return: returns 0 if NULL or number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
+	if (!filename)
+	{
+		return (0);
+	}
+
+	file_descriptor = open(filename, O_RDONLY);
+
+	if (file_descriptor == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * (letters));
+
+	if (!buffer)
+	{
+		return (0);
+	}
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, nrd);
+
+	close(file_descriptor);
+
+	free(buffer);
+
+	return (bytes_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,9 @@
+**0x15. C - File I/O**
+
+The essence of this project is to learn 
+-various commands for creating and using files
+-file descriptors
+-the 3 standard file descriptors
+-How to use the I/O system calls open, close, read and write
+-flags O_RDONLY, O_WRONLY, O_RDWR, what they are and how to use them
+-the difference between a function and a system call
