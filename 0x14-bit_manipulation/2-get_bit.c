f76fb92472e929e0667ed06b7a824240df7c3ec2
KwameNtaadu007 <kwamealabama@gmail.com>
2023-04-10 14:08:18 -0700

    betyy corrections

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * 
  * get_bit - returns the value of a bit at a given index
  * @n: interger input
  * @index: index of the bit.
@@ -11,10 +10,9 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-
 	if (index >= sizeof(n) * 8)
 	{
-        	return -1; /* index out of bounds*/
-    	}
-    	return (n >> index) & 1;
+		return (-1); /*index out of bounds*/
+	}
+	return ((n >> index) & 1);
 }
