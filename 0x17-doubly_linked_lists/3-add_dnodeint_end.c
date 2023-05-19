328e9a8f131b855731de98d0262fa6180a7e8100
KwameNtaadu007 <kwamealabama@gmail.com>
2023-05-19 01:00:33 -0700

    free memory of a linked list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	/* Create a new node */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	/* Set the value of the new node */
 	new_node->n = n;
@@ -39,6 +39,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		new_node->next = NULL;
 	}
 
-	return new_node;
+	return (new_node);
 }
 
