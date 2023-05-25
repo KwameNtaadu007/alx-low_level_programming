073f664edf6e479276ab18565d255e251a6bf0ba
KwameNtaadu007 <kwamealabama@gmail.com>
2023-05-25 13:01:43 -0700

    past present future

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,6 +20,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *);
+char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* _MAIN_H_ */
